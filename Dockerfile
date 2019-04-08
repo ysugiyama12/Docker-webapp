027ad8bacfd8b41a04dba9bddfc3ad3c160949ed
ysugiyama12 <yuji9511yuji@gmail.com>
2019-04-08 22:31:16 +0900

    herokuへのアップロード

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,14 @@
-FROM golang:latest
+FROM golang:latest as builder
 
-WORKDIR /go
-ADD . /go
+ENV CGO_ENABLED=0
+ENV GOOS=linux
+ENV GOARCH=amd64
+WORKDIR /go/src/github.com/yokoe/go-server-example
+COPY . .
+RUN go build main.go
 
-CMD ["go", "run", "main.go"]
+# runtime image
+FROM alpine
+COPY --from=builder /go/src/github.com/yokoe/go-server-example /app
+
+CMD /app/main $PORT
--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import(
-	"log"
+	// "log"
 	"net/http"
 	// "path/filepath"
 	"sync"
 	"text/template"
-	// "fmt"
+	"fmt"
 	"encoding/json"
+	"strconv"
+	"os"
 )
 
 type templateHandler struct {
@@ -39,8 +41,8 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.Handle("/", &templateHandler{})
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal("ListenAndServe", err)
-	}
+    port, _ := strconv.Atoi(os.Args[1])
+    fmt.Printf("Starting server at Port %d", port)
+    http.Handle("/", &templateHandler{})
+    http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
